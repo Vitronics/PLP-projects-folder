fd623080943f047e1579dc0809a83cf36c2bbf76
Vitronics <141555452+Vitronics@users.noreply.github.com>
2024-05-28 21:14:03 +0300

    Add files via upload

---
+++ favourite_books.py
@@ -0,0 +1,6 @@
+Books = ("The Bible","Rich Dad Poor Dad", "Power of Your Subconcoius Mind", 
+         "The Richest Man in Babylon","The Atomic Habit","The Magic of Thinking Big",
+         "The 4AM Club, " )
+for Books in Books:
+  print(Books)
+
