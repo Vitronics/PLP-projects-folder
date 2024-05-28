fce4ac9bc6f5ae8eba777654f587753a2c665c5b
Vitronics <141555452+Vitronics@users.noreply.github.com>
2024-05-28 21:25:02 +0300

    Update favourite_books.py

--- favourite_books.py
+++ favourite_books.py
@@ -1,3 +1,4 @@
+#A program that Creates a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
 Books = ("The Bible","Rich Dad Poor Dad", "Power of Your Subconcoius Mind", 
          "The Richest Man in Babylon","The Atomic Habit","The Magic of Thinking Big",
          "The 4AM Club, " )
